9d7a0a0e6a1b536d521f85498257ff31c4f8d50e
Jens Malmborg <jnsmalm@gmail.com>
2019-10-03 21:49:14 +0200

    Initial development setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path")
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin")
+
+module.exports = env => {
+  return {
+    entry: "./src/index.ts",
+    mode: mode(env),
+    devtool: devtool(env),
+    plugins: [
+      new BrowserSyncPlugin({
+        host: "localhost",
+        port: 3000,
+        server: { baseDir: ["dist"] },
+        watch: true
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".ts", ".js"]
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "pixi3d.js",
+      library: "PIXI3D",
+      libraryTarget: "umd",
+      umdNamedDefine: true
+    }
+  }
+}
+
+const mode = env => {
+  return env.production ? "production" : "development"
+}
+
+const devtool = env => {
+  return env.production ? "" : "inline-source-map"
+}
