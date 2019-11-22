a37f3ed3aa936bfc54e69b6b0863a46712adfbb9
Jens Malmborg <jnsmalm@gmail.com>
2019-11-22 20:25:42 +0100

    Mesh shader makes it easier to create geometry

--- src/shader.ts
+++ src/shader.ts
@@ -6,3 +6,54 @@ export interface Shader extends PIXI.Shader {
   createGeometry(data: MeshGeometryData): PIXI.Geometry
   createMaterial?(material?: Material): Material
 }
+
+export enum MeshShaderAttribute {
+  position = "position",
+  uv = "uv",
+  normal = "normal",
+  tangent = "tangent"
+}
+
+export abstract class MeshShader extends PIXI.Shader implements Shader {
+  constructor(program: PIXI.Program, public attributes: MeshShaderAttribute[] = []) {
+    super(program)
+  }
+
+  createGeometry(data: MeshGeometryData): PIXI.Geometry {
+    let geometry = new PIXI.Geometry()
+    if (data.indices) {
+      geometry.addIndex(data.indices.buffer)
+    }
+    if (this.attributes.includes(MeshShaderAttribute.position)) {
+      geometry.addAttribute("position", data.positions.buffer, 3, false,
+        PIXI.TYPES.FLOAT, data.positions.stride)
+    }
+    if (this.attributes.includes(MeshShaderAttribute.uv)) {
+      if (data.texCoords) {
+        geometry.addAttribute("uv", data.texCoords.buffer, 2, false,
+          PIXI.TYPES.FLOAT, data.texCoords.stride)
+      }
+    }
+    if (this.attributes.includes(MeshShaderAttribute.normal)) {
+      if (data.normals) {
+        geometry.addAttribute("normal", data.normals.buffer, 3, false,
+          PIXI.TYPES.FLOAT, data.normals.stride)
+      }
+    }
+    if (this.attributes.includes(MeshShaderAttribute.normal)) {
+      if (data.normals) {
+        geometry.addAttribute("normal", data.normals.buffer, 3, false,
+          PIXI.TYPES.FLOAT, data.normals.stride)
+      }
+    }
+    if (this.attributes.includes(MeshShaderAttribute.tangent)) {
+      if (data.tangents) {
+        geometry.addAttribute("tangent", data.tangents.buffer, 4, false,
+          PIXI.TYPES.FLOAT, data.tangents.stride)
+      }
+    }
+    return geometry
+  }
+
+  abstract updateUniforms(mesh: Mesh3D): void
+}
