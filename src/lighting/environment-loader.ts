fba0f0a6c101f551b4a8e8218c26137a16468348
Jens Malmborg <jnsmalm@gmail.com>
2019-11-30 22:47:25 +0100

    Added support for loading lighting environment

---
+++ src/lighting/environment-loader.ts
@@ -0,0 +1,23 @@
+import { CubeMipMapTexture } from "../cubemap/cube-mipmap"
+import { LightingEnvironment } from "../light"
+
+const EXTENSION = "lightenv"
+
+export const EnvironmentLoader = {
+  use: (resource: any, next: () => void) => {
+    if (resource.extension !== EXTENSION) {
+      return next()
+    }
+    let lightingEnvironment = new LightingEnvironment()
+    lightingEnvironment.radianceTexture =
+      new CubeMipMapTexture(resource.data.specular)
+    lightingEnvironment.irradianceTexture =
+      new CubeMipMapTexture(resource.data.diffuse)
+    resource.lightingEnvironment = lightingEnvironment
+    next()
+  },
+  add: () => {
+    PIXI.LoaderResource.setExtensionXhrType(
+      EXTENSION, PIXI.LoaderResource.XHR_RESPONSE_TYPE.JSON)
+  }
+}
