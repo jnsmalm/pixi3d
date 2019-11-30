383e07302ca973fbaaf85d42207bc28a283014fc
Jens Malmborg <jnsmalm@gmail.com>
2019-11-30 20:51:39 +0100

    Refactoring for cubemap loader

--- src/cubemap/cube-mipmap.ts
+++ src/cubemap/cube-mipmap.ts
@@ -1,3 +1,39 @@
+import { CubeMapResource } from "./cubemap-loader"
+import { ImageMipMapResource } from "./image-mipmap"
+
+export class CubeMipMapTexture extends PIXI.CubeTexture {
+  constructor(resource: CubeMapResource) {
+    let faces = createFacesResource(resource)
+    if (resource.mipmap && resource.mipmap.length > 0) {
+      ImageMipMapResource.install()
+      super(new CubeMipMapResource(faces, resource.mipmap.length))
+    } else {
+      super(new PIXI.resources.CubeResource(
+        faces.map((value) => { return value.source })))
+    }
+  }
+}
+
+const FACES = ["posx", "negx", "posy", "negy", "posz", "negz"]
+
+function createFacesResource(data: CubeMapResource): CubeMapResource[] {
+  return FACES.map((value) => {
+    let result: CubeMapResource = {
+      source: data.source.replace("{{face}}", value),
+      mipmap: []
+    }
+    if (data.mipmap && data.mipmap.length > 0) {
+      for (let i = 0; i < data.mipmap.length; i++) {
+        let url = data.mipmap[i].replace("{{face}}", value)
+        if (result.mipmap) {
+          result.mipmap.push(url)
+        }
+      }
+    }
+    return result
+  })
+}
+
 export class CubeMipMapResource extends PIXI.resources.CubeResource {
   constructor(resources: any, public levels: number) {
     super(resources)
--- src/cubemap/cubemap-loader.ts
+++ src/cubemap/cubemap-loader.ts
@@ -1,5 +1,4 @@
-import { CubeMipMapResource } from "./cube-mipmap"
-import { ImageMipMapResource } from "./image-mipmap"
+import { CubeMipMapTexture } from "./cube-mipmap"
 
 export interface CubeMapResource { source: string, mipmap?: string[] }
 
@@ -8,39 +7,11 @@ export const CubeMapLoader = {
     if (resource.extension !== "cubemap") {
       return next()
     }
-    let faces = createFacesResource(resource.data)
-    if (resource.data.mipmap) {
-      ImageMipMapResource.install()
-      resource.texture = new PIXI.CubeTexture(
-        new CubeMipMapResource(faces, resource.data.mipmap.length))
-    } else {
-      resource.texture = PIXI.CubeTexture.from(
-        faces.map((value) => { return value.source }))
-    }
+    resource.texture = new CubeMipMapTexture(resource.data)
     next()
   },
   add: () => {
     PIXI.LoaderResource.setExtensionXhrType(
       "cubemap", PIXI.LoaderResource.XHR_RESPONSE_TYPE.JSON)
   }
-}
-
-const FACES = ["posx", "negx", "posy", "negy", "posz", "negz"]
-
-function createFacesResource(data: CubeMapResource): CubeMapResource[] {
-  return FACES.map((value) => {
-    let result: CubeMapResource = {
-      source: data.source.replace("{{face}}", value),
-      mipmap: []
-    }
-    if (data.mipmap && data.mipmap.length > 0) {
-      for (let i = 0; i < data.mipmap.length; i++) {
-        let url = data.mipmap[i].replace("{{face}}", value)
-        if (result.mipmap) {
-          result.mipmap.push(url)
-        }
-      }
-    }
-    return result
-  })
 }
