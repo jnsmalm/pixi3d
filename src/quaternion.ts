d41a7b02fc89f6573437bde513fcacac476c7f21
Jens Malmborg <jnsmalm@gmail.com>
2020-02-03 20:31:04 +0100

    Docs for quaternion

--- src/quaternion.ts
+++ src/quaternion.ts
@@ -3,6 +3,9 @@ import { Quaternion } from "./math/quaternion"
 
 const quat = Quaternion.create()
 
+/**
+ * Quaternions represents rotations in 3D space.
+ */
 export class ObservableQuaternion extends ObservablePoint3D {
   _w: number
 
@@ -11,6 +14,12 @@ export class ObservableQuaternion extends ObservablePoint3D {
     this._w = w
   }
 
+  /**
+   * Sets the euler angle in degrees.
+   * @param x X angle.
+   * @param y Y angle.
+   * @param z Z angle.
+   */
   setEulerAngles(x: number, y: number, z: number) {
     Quaternion.fromEuler(x, y, z, quat)
     if (this._x !== quat[0] || this._y !== quat[1] || this._z !== quat[2] || this._w !== quat[3]) {
@@ -22,6 +31,7 @@ export class ObservableQuaternion extends ObservablePoint3D {
     }
   }
 
+  /** W component of the quaternion. */
   get w() {
     return this._w
   }
@@ -33,11 +43,20 @@ export class ObservableQuaternion extends ObservablePoint3D {
     }
   }
 
+  /**
+   * Creates a clone of this quaternion.
+   * @param cb Callback when changed.
+   * @param scope Owner of callback.
+   */
   clone(cb: (() => void) | null = null, scope: any) {
     return new ObservableQuaternion(
       cb || this.cb, scope || this.scope, this._x, this._y, this._z, this._w)
   }
 
+  /**
+   * Copies x, y, z, and w from the given quaternion.
+   * @param p The quaternion to copy from.
+   */
   copyFrom(p: { x: number, y: number, z: number, w: number } | Float32Array) {
     if (ArrayBuffer.isView(p)) {
       p = { x: p[0], y: p[1], z: p[2], w: p[3] }
@@ -52,16 +71,32 @@ export class ObservableQuaternion extends ObservablePoint3D {
     return this
   }
 
+  /**
+   * Copies x, y, z and w into the given quaternion.
+   * @param p The quaternion to copy to.
+   */
   copyTo(p: ObservableQuaternion) {
     p.set(this._x, this._y, this._z, this._w)
     return p
   }
 
+  /**
+   * Returns true if the given quaternion is equal to this quaternion.
+   * @param p The quaternion to check.
+   */
   equals(p: ObservableQuaternion) {
     return (p.x === this._x) && (p.y === this._y) && (p.z === this._z) && (p.w === this._w)
   }
 
   set(...xyzw: number[]): void
+
+  /**
+   * Sets the quaternion to a new x, y,z and w position.
+   * @param x X component to set.
+   * @param y Y component to set.
+   * @param z Z component to set.
+   * @param w W component to set.
+   */
   set(x: number, y?: number, z?: number, w?: number) {
     const _x = x || 0
     const _y = y || ((y !== 0) ? _x : 0)
