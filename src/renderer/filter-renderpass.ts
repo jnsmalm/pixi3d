79fbca4f4badcd1a45b982ab09d3c0219206b8b5
Jens Malmborg <jnsmalm@gmail.com>
2020-06-29 15:30:25 +0200

    Rafactoring for filter render pass

--- src/renderer/filter-renderpass.ts
+++ src/renderer/filter-renderpass.ts
@@ -5,42 +5,27 @@ import { MeshRenderPass } from "./mesh-renderpass"
 import { MeshShader } from "../mesh/mesh-shader"
 import { MeshGeometry } from "../mesh/mesh-geometry"
 
-interface FilterRenderPassOptions {
-  input?: PIXI.Texture
-  width?: number
-  height?: number
-  format?: PIXI.FORMATS
-  type?: PIXI.TYPES
-}
-
 /**
  * Render pass used for post processing filters.
  */
 export class FilterRenderPass implements MeshRenderPass {
   private _mesh: Mesh3D
+  private _outputTexture?: PIXI.RenderTexture
+  private _outputTextureAutoResize = true
 
-  renderToTexture = false
-
-  /** Texture input to this render pass. */
-  input?: PIXI.Texture
-
-  /** Texture output from this render pass. */
-  output: PIXI.RenderTexture
+  /** Gets the output render texture. */
+  get outputTexture() {
+    return this._outputTexture
+  }
 
   /**
    * Creates a new filter rendering pass.
    * @param renderer Renderer to use.
+   * @param name Name for this render pass.
    * @param shader Shader to use for filter.
    * @param input Input texture to use for filter.
    */
-  constructor(public renderer: PIXI.Renderer, public name: string, public shader: MeshShader, options: FilterRenderPassOptions = {}) {
-    let { input, width = 256, height = 256, format = PIXI.FORMATS.RGBA, type = PIXI.TYPES.UNSIGNED_BYTE } = options
-
-    this.input = input
-
-    // @ts-ignore Format does exist in the options
-    this.output = PIXI.RenderTexture.create({ width, height, format, type })
-
+  constructor(public renderer: PIXI.Renderer, public name: string, public shader: MeshShader, public input?: PIXI.Texture) {
     this._mesh = new Mesh3D(Object.assign(new MeshGeometry(), {
       indices: {
         buffer: new Uint8Array([0, 1, 2, 0, 3, 1]),
@@ -51,18 +36,32 @@ export class FilterRenderPass implements MeshRenderPass {
         stride: 0
       }
     }))
+    renderer.on("prerender", () => {
+      if (this._outputTexture && this._outputTextureAutoResize) {
+        this._outputTexture.resize(renderer.width, renderer.height)
+      }
+    })
+  }
+
+  enableRenderToTexture(options: { width?: number, height?: number, format?: PIXI.FORMATS, type?: PIXI.TYPES } = {}) {
+    let { width = 512, height = 512, format, type } = options
+    if (options && options.width && options.height) {
+      this._outputTextureAutoResize = false
+    }
+    // @ts-ignore format does exist in options
+    this._outputTexture = PIXI.RenderTexture.create({ width, height, format, type })
   }
 
   /**
    * Renders this post processing filter pass.
    */
   render() {
-    if (this.renderToTexture) {
-      this.renderer.renderTexture.bind(this.output)
+    if (this._outputTexture) {
+      this.renderer.renderTexture.bind(this._outputTexture)
     }
     this.updateUniforms()
     this.shader.render(this._mesh, this.renderer)
-    if (this.renderToTexture) {
+    if (this._outputTexture) {
       this.renderer.renderTexture.bind(undefined)
     }
   }
@@ -74,14 +73,14 @@ export class FilterRenderPass implements MeshRenderPass {
     // @ts-ignore uniformData does exist on program
     let uniformData = this.shader.program.uniformData
 
-    if (uniformData.u_OutputSize) {
-      if (this.renderToTexture) {
-        this.shader.uniforms.u_OutputSize = [
-          this.output.width,
-          this.output.height
+    if (uniformData.u_ViewSize) {
+      if (this._outputTexture) {
+        this.shader.uniforms.u_ViewSize = [
+          this._outputTexture.width,
+          this._outputTexture.height
         ]
       } else {
-        this.shader.uniforms.u_OutputSize = [
+        this.shader.uniforms.u_ViewSize = [
           this.renderer.width,
           this.renderer.height
         ]
@@ -96,7 +95,7 @@ export class FilterRenderPass implements MeshRenderPass {
    * Clears the filter pass before rendering.
    */
   clear() {
-    this.renderer.renderTexture.bind(this.output)
+    this.renderer.renderTexture.bind(this._outputTexture)
     this.renderer.renderTexture.clear()
     this.renderer.renderTexture.bind(undefined)
   }
@@ -108,8 +107,8 @@ export class FilterRenderPass implements MeshRenderPass {
    * @param fragmentSource Fragment shader source.
    * @param input Input texture to use for filter.
    */
-  static from(renderer: PIXI.Renderer, name: string, vertexSource: string, fragmentSource: string, options: FilterRenderPassOptions = {}) {
-    return new FilterRenderPass(
-      renderer, name, new MeshShader(PIXI.Program.from(vertexSource, fragmentSource)), options)
+  static from(renderer: PIXI.Renderer, name: string, vertexSource: string, fragmentSource: string, input?: PIXI.Texture) {
+    return new FilterRenderPass(renderer, name,
+      new MeshShader(PIXI.Program.from(vertexSource, fragmentSource)), input)
   }
 }
