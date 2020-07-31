74fda4ca7e125c26bf455754b82f9fe3d082949a
Jens Malmborg <jnsmalm@gmail.com>
2020-07-31 10:41:16 +0200

    Specific typed arrays for mesh geometry attribute

--- src/mesh/geometry/mesh-geometry-attribute.ts
+++ src/mesh/geometry/mesh-geometry-attribute.ts
@@ -1,4 +1,4 @@
 export interface MeshGeometryAttribute {
-    buffer: ArrayBuffer
+    buffer: Uint32Array | Float32Array | Int8Array | Uint8Array | Int16Array | Uint16Array
     stride: number | undefined
 }
