7f2665c34827fa50730a1931fcbd4aa29a426243
Jens Malmborg <jnsmalm@gmail.com>
2021-05-08 14:19:27 +0200

    Fix for model viewer example

--- examples/src/model-viewer.js
+++ examples/src/model-viewer.js
@@ -113,8 +113,8 @@ app.loader.add("specular.cubemap", "assets/environments/autumn/specular.cubemap"
 app.loader.load((loader, resources) => {
   PIXI3D.LightingEnvironment.main =
     new PIXI3D.LightingEnvironment(app.renderer, new PIXI3D.ImageBasedLighting(
-      resources["diffuse.cubemap"].texture,
-      resources["specular.cubemap"].texture))
+      resources["diffuse.cubemap"].cubemap,
+      resources["specular.cubemap"].cubemap))
 })
 
 let material = {
