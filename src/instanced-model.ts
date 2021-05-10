709dab0c51f495ca623368f55361e6693b47bd1c
Jens Malmborg <jnsmalm@gmail.com>
2021-05-10 20:32:46 +0200

    Added destroy for instanced model

--- src/instanced-model.ts
+++ src/instanced-model.ts
@@ -1,11 +1,20 @@
-import { Container3D } from "./container"
+import * as PIXI from "pixi.js"
+
 import { InstancedMesh3D } from "./mesh/instanced-mesh"
+import { Container3D } from "./container"
 
 export class InstancedModel extends Container3D {
   constructor(readonly meshes: InstancedMesh3D[]) {
     super()
     for (let mesh of meshes) {
       this.addChild(mesh)
+    }
+  }
+
+  destroy(options: boolean | PIXI.IDestroyOptions | undefined) {
+    super.destroy(options)
+    for (let mesh of this.meshes) {
+      mesh.destroy(options)
     }
   }
 }
