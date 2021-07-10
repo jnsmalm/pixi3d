4d42776dfbe91a9a7b8fa829110e7e8b0909d196
Jens Malmborg <jnsmalm@gmail.com>
2021-07-10 17:02:52 +0200

    Fixed shader for Safari with WebGL 1.0

--- src/material/standard/shader/texture-lod.glsl
+++ src/material/standard/shader/texture-lod.glsl
@@ -2,7 +2,8 @@ vec4 _textureLod(sampler2D sampler, vec2 coord, float lod)
 {
 #ifdef WEBGL2
     return textureLod(sampler, coord, lod);
-#elif GL_EXT_shader_texture_lod
+#endif
+#if defined(WEBGL1) && defined(GL_EXT_shader_texture_lod) 
     return texture2DLodEXT(sampler, coord, lod);
 #endif
     return vec4(0.0);
@@ -12,7 +13,8 @@ vec4 _textureLod(samplerCube sampler, vec3 coord, float lod)
 {
 #ifdef WEBGL2
     return textureLod(sampler, coord, lod);
-#elif GL_EXT_shader_texture_lod
+#endif
+#if defined(WEBGL1) && defined(GL_EXT_shader_texture_lod) 
     return textureCubeLodEXT(sampler, coord, lod);
 #endif
     return vec4(0.0);
