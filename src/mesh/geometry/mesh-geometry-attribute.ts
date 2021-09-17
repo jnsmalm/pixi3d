1bcc50aa8d2e3beac7b715ee7c4834187eda8790
Jens Malmborg <jnsmalm@gmail.com>
2021-09-17 22:54:13 +0200

    Docs for mesh geometry attribute

--- src/mesh/geometry/mesh-geometry-attribute.ts
+++ src/mesh/geometry/mesh-geometry-attribute.ts
@@ -1,7 +1,23 @@
+/**
+ * Represents an attribute for mesh geometry.
+ */
 export interface MeshGeometryAttribute {
-    buffer: Uint32Array | Float32Array | Int8Array | Uint8Array | Int16Array | Uint16Array
+    /**
+     * The stride, in bytes, between attributes. When this is not defined, data
+     * is tightly packed. When two or more attributes use the same buffer, this
+     * field must be defined.
+     */
     stride?: number
-    componentType?: number
+
+    /** The buffer data. */
+    buffer: Uint32Array | Float32Array | Int8Array | Uint8Array | Int16Array | Uint16Array
+
+    /** The minimum value of each component in this attribute. */
     min?: number[]
+
+    /** The maximum value of each component in this attribute. */
     max?: number[]
+
+    /** The datatype of components in this attribute. */
+    componentType?: number
 }
