e2f6bf693ebfb40b67a118dcb13cfeea4e1aa5b9
Jens Malmborg <jnsmalm@gmail.com>
2021-09-19 20:13:55 +0200

    Export mesh geometry target

--- src/mesh/geometry/mesh-geometry-attribute.ts
+++ src/mesh/geometry/mesh-geometry-attribute.ts
@@ -3,21 +3,25 @@
  */
 export interface MeshGeometryAttribute {
     /**
+     * The buffer data.
+     */
+    buffer: Uint32Array | Float32Array | Int8Array | Uint8Array | Int16Array | Uint16Array
+    /**
+     * The minimum value of each component in this attribute.
+     */
+    min?: number[]
+    /**
+     * The maximum value of each component in this attribute.
+     */
+    max?: number[]
+    /**
+     * The datatype of components in this attribute.
+     */
+    componentType?: number
+    /**
      * The stride, in bytes, between attributes. When this is not defined, data
      * is tightly packed. When two or more attributes use the same buffer, this
      * field must be defined.
      */
-    stride?: number
-
-    /** The buffer data. */
-    buffer: Uint32Array | Float32Array | Int8Array | Uint8Array | Int16Array | Uint16Array
-
-    /** The minimum value of each component in this attribute. */
-    min?: number[]
-
-    /** The maximum value of each component in this attribute. */
-    max?: number[]
-
-    /** The datatype of components in this attribute. */
-    componentType?: number
+     stride?: number
 }
--- src/mesh/geometry/mesh-geometry-target.ts
+++ src/mesh/geometry/mesh-geometry-target.ts
@@ -1,5 +1,8 @@
 import { MeshGeometryAttribute } from "./mesh-geometry-attribute"
 
+/**
+ * Represents a geometry morph target.
+ */
 export interface MeshGeometryTarget {
     positions?: MeshGeometryAttribute
     normals?: MeshGeometryAttribute
