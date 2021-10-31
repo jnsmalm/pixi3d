1f838360c1a543d9a0e11e5f09d2c731266a9d03
Jens Malmborg <jnsmalm@gmail.com>
2021-10-31 14:31:12 +0100

    Draw webgl canvas to 2d canvas for tests

--- test/test-utils.js
+++ test/test-utils.js
@@ -9,17 +9,12 @@ async function getObjectURLFromRender(render, resources, width = 1280, height = 
   let result = new Promise((resolve, reject) => {
     loader.load((_, resources) => {
       render(renderer, resources)
-      // Sprite is added as watermark to be able to tell orientation of the
-      // image, due to a bug in Safari where the image is read upside-down.
-      let watermark = Object.assign(
-        new PIXI.Sprite(PIXI.Texture.WHITE), { width: 1, height: 1, })
-      if (PIXI.VERSION.substring(0, 1) === "5") {
-        renderer.render(watermark, undefined, false)
-      }
-      if (PIXI.VERSION.substring(0, 1) === "6") {
-        renderer.render(watermark, { clear: false })
-      }
-      renderer.view.toBlob(blob => {
+      let canvas = document.createElement("canvas")
+      canvas.width = width
+      canvas.height = height
+      let ctx = canvas.getContext("2d")
+      ctx.drawImage(renderer.view, 0, 0)
+      canvas.toBlob(blob => {
         resolve(URL.createObjectURL(blob))
         renderer.destroy()
         PIXI.utils.clearTextureCache()
@@ -29,7 +24,7 @@ async function getObjectURLFromRender(render, resources, width = 1280, height = 
   return result
 }
 
-export async function getImageDataFromUrl(url, flipped) {
+export async function getImageDataFromUrl(url) {
   return new Promise((resolve, reject) => {
     let image = new Image()
     image.src = url
@@ -38,10 +33,6 @@ export async function getImageDataFromUrl(url, flipped) {
       canvas.width = image.width
       canvas.height = image.height
       let ctx = canvas.getContext("2d")
-      if (flipped) {
-        ctx.translate(0, canvas.height)
-        ctx.scale(1, -1)
-      }
       ctx.drawImage(image, 0, 0)
       let imageData = ctx.getImageData(0, 0, image.width, image.height)
       resolve({
@@ -55,11 +46,6 @@ export async function getImageDataFromUrl(url, flipped) {
 }
 
 export async function getImageDataFromRender(render, resources, width = 1280, height = 720) {
-  let url = await getObjectURLFromRender(render, resources, width, height)
-  let imageData = await getImageDataFromUrl(url, false)
-  let flipped = imageData.data[0] !== 255
-  if (flipped) {
-    imageData = await getImageDataFromUrl(url, true)
-  }
-  return imageData
+  return await getImageDataFromUrl(
+    await getObjectURLFromRender(render, resources, width, height), false)
 }
