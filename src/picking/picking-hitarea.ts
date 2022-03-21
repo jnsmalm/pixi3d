955fa3439ec6c2eba4829cc40537123cc41d9705
Karthik VJ <karthikeyanvj@gmail.com>
2022-03-21 22:54:27 +0200

    Added static function to create picking hit area
    * added static function to create PickingHitArea
    
    * updated comment
    
    Co-authored-by: karthikeyan.jayakuma <karthikeyan.jayakumar@gamesys.ee>

--- src/picking/picking-hitarea.ts
+++ src/picking/picking-hitarea.ts
@@ -28,4 +28,12 @@ export class PickingHitArea implements IHitArea {
   contains(x: number, y: number) {
     return this._picking.containsHitArea(x, y, this)
   }
+
+  /**
+   * Creates a new hitarea using the specified object with camera's main renderer
+   * @param object
+   */
+  static fromObject(object: Mesh3D | Model): PickingHitArea {
+    return new PickingHitArea(Camera.main.renderer, object)
+  }
 }
