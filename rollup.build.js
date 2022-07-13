138acd2a6333e925b5b35b996a85c46dcee8b133
gordontombola <gordon.cummings@tombola.com>
2022-07-13 13:54:34 +0100

    change esm target to es5
    failure to extends classes from pixi3d when using es5 and esm if the target is set to es2017

--- rollup.build.js
+++ rollup.build.js
@@ -8,83 +8,83 @@ import { terser } from "rollup-plugin-terser"
 
 const globals = {}
 Object.keys(pkg.peerDependencies).forEach(function (key) {
-  const { namespace = "PIXI" } = require(`${key}/package.json`)
-  globals[key] = namespace
+    const { namespace = "PIXI" } = require(`${key}/package.json`)
+    globals[key] = namespace
 })
 
 const external = [
-  "@pixi/loaders",
-  "@pixi/core",
-  "@pixi/math",
-  "@pixi/display",
-  "@pixi/constants",
-  "@pixi/ticker",
-  "@pixi/utils",
-  "@pixi/sprite",
-  "@pixi/interaction",
-  "@pixi/settings"
+    "@pixi/loaders",
+    "@pixi/core",
+    "@pixi/math",
+    "@pixi/display",
+    "@pixi/constants",
+    "@pixi/ticker",
+    "@pixi/utils",
+    "@pixi/sprite",
+    "@pixi/interaction",
+    "@pixi/settings"
 ]
 
 const banner = `/* Pixi3D v${pkg.version} */`
 const minify = terser({
-  output: {
-    comments: (_, comment) => comment.line === 1
-  }
+    output: {
+        comments: (_, comment) => comment.line === 1
+    }
 })
 
 const plugins = (target) => {
-  return [
-    typescript({ target, noEmitOnError: true }),
-    image(),
-    glsl(),
-    resolve()
-  ]
+    return [
+        typescript({ target, noEmitOnError: true }),
+        image(),
+        glsl(),
+        resolve()
+    ]
 }
 
 export default [{
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/umd/pixi3d.js",
-      format: "umd",
-      sourcemap: true,
-      name: "PIXI3D",
-      globals,
-      banner
-    },
-    {
-      file: "dist/umd/pixi3d.min.js",
-      format: "umd",
-      sourcemap: true,
-      name: "PIXI3D",
-      globals,
-      banner,
-      plugins: [
-        minify
-      ]
-    }
-  ],
-  external,
-  plugins: plugins("es5")
+    input: "src/index.ts",
+    output: [
+        {
+            file: "dist/umd/pixi3d.js",
+            format: "umd",
+            sourcemap: true,
+            name: "PIXI3D",
+            globals,
+            banner
+        },
+        {
+            file: "dist/umd/pixi3d.min.js",
+            format: "umd",
+            sourcemap: true,
+            name: "PIXI3D",
+            globals,
+            banner,
+            plugins: [
+                minify
+            ]
+        }
+    ],
+    external,
+    plugins: plugins("es5")
 }, {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/esm/pixi3d.js",
-      format: "esm",
-      sourcemap: true,
-      banner
-    },
-    {
-      file: "dist/esm/pixi3d.min.js",
-      format: "esm",
-      sourcemap: true,
-      banner,
-      plugins: [
-        minify
-      ]
-    }
-  ],
-  external,
-  plugins: plugins("es2017")
+    input: "src/index.ts",
+    output: [
+        {
+            file: "dist/esm/pixi3d.js",
+            format: "esm",
+            sourcemap: true,
+            banner
+        },
+        {
+            file: "dist/esm/pixi3d.min.js",
+            format: "esm",
+            sourcemap: true,
+            banner,
+            plugins: [
+                minify
+            ]
+        }
+    ],
+    external,
+    plugins: plugins("es5")
 }]
