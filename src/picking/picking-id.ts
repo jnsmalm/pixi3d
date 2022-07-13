cfe3dd3c85803040cdd79f7b50f8407ab1d54bad
gordontombola <gordon.cummings@tombola.com>
2022-07-13 14:11:40 +0100

    removed graphics based picking

--- src/picking/picking-id.ts
+++
@@ -1,9 +0,0 @@
-export namespace PickingId {
-  let id = 0
-  export function next() {
-    id++
-    return new Uint8Array([
-      (id >> 16) & 255, (id >> 8) & 255, id & 255
-    ])
-  }
-}
--- src/picking/shader/picking.vert
+++
@@ -1,9 +0,0 @@
-#version 100
-
-attribute vec3 a_Position;
-
-uniform mat4 u_ModelViewProjection;
-
-void main() {
-  gl_Position = u_ModelViewProjection * vec4(a_Position, 1.0);
-}
