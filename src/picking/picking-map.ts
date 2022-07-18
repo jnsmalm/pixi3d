1ddb4a21c31e7f88d4bc7b5f1ff7a36ca73ba559
gordontombola <gordon.cummings@tombola.com>
2022-07-18 14:56:00 +0100

    tab sizes

--- src/picking/picking-map.ts
+++ src/picking/picking-map.ts
@@ -9,75 +9,75 @@ import { Shader as Fragment } from "./shader/picking.frag"
 import { settings } from "@pixi/settings"
 
 export class PickingMap {
-    private _pixels: Uint8Array
-    private _output: RenderTexture
-    private _shader: MeshShader
-    private _update = 0
+  private _pixels: Uint8Array
+  private _output: RenderTexture
+  private _shader: MeshShader
+  private _update = 0
 
-    constructor(private _renderer: Renderer, size: number) {
-        this._pixels = new Uint8Array(size * size * 4)
-        this._output = RenderTexture.create({ width: size, height: size })
-        this._shader = new MeshShader(Program.from(Vertex.source, Fragment.source))
-        this._output.framebuffer.addDepthTexture()
+  constructor(private _renderer: Renderer, size: number) {
+    this._pixels = new Uint8Array(size * size * 4)
+    this._output = RenderTexture.create({ width: size, height: size })
+    this._shader = new MeshShader(Program.from(Vertex.source, Fragment.source))
+    this._output.framebuffer.addDepthTexture()
+  }
+
+  destroy() {
+    this._output.destroy(true)
+    this._shader.destroy()
+  }
+
+  resizeToAspect() {
+    const aspect = this._renderer.width / this._renderer.height
+    const aspectWidth = Math.floor(this._output.height * aspect)
+    if (this._output.width !== aspectWidth) {
+      this._pixels = new Uint8Array(aspectWidth * this._output.height * 4)
+      this._output.resize(aspectWidth, this._output.height)
     }
+  }
 
-    destroy() {
-        this._output.destroy(true)
-        this._shader.destroy()
+  containsId(x: number, y: number, id: Uint8Array) {
+    const { width, height } = this._renderer.screen
+
+    x = Math.floor(x / width * this._output.width)
+    y = Math.floor((height - y) / height * this._output.height)
+    for (let i = 0; i < 3; i++) {
+      if (id[i] !== this._pixels[(y * this._output.width + x) * 4 + i]) {
+        return false
+      }
     }
+    return true
+  }
 
-    resizeToAspect() {
-        const aspect = this._renderer.width / this._renderer.height
-        const aspectWidth = Math.floor(this._output.height * aspect)
-        if (this._output.width !== aspectWidth) {
-            this._pixels = new Uint8Array(aspectWidth * this._output.height * 4)
-            this._output.resize(aspectWidth, this._output.height)
-        }
+  update(hitAreas: PickingHitArea[]) {
+    const oldRes = settings.RESOLUTION;
+    settings.RESOLUTION = 1;
+    this._renderer.renderTexture.bind(this._output)
+    if (this._update++ % 2 === 0) {
+      // For performance reasons, the update method alternates between rendering 
+      // the meshes and reading the pixels from the rendered texture.
+      this._renderer.renderTexture.clear()
+      for (let hitArea of hitAreas) {
+        this.renderHitArea(hitArea)
+      }
+    } else {
+      const gl = this._renderer.gl
+      gl.readPixels(0, 0, this._output.width, this._output.height, gl.RGBA, gl.UNSIGNED_BYTE, this._pixels)
     }
+    settings.RESOLUTION = oldRes;
+    this._renderer.renderTexture.bind(undefined)
+  }
 
-    containsId(x: number, y: number, id: Uint8Array) {
-        const { width, height } = this._renderer.screen
+  private _matrix = new Float32Array(16)
 
-        x = Math.floor(x / width * this._output.width)
-        y = Math.floor((height - y) / height * this._output.height)
-        for (let i = 0; i < 3; i++) {
-            if (id[i] !== this._pixels[(y * this._output.width + x) * 4 + i]) {
-                return false
-            }
-        }
-        return true
+  renderHitArea(hitArea: PickingHitArea) {
+    const uniforms = this._shader.uniforms
+    const meshes = hitArea.object instanceof Mesh3D ? [hitArea.object] : hitArea.object.meshes
+    const camera = hitArea.camera || Camera.main
+    for (let mesh of meshes) {
+      uniforms.u_Id = hitArea.id
+      uniforms.u_ModelViewProjection = Mat4.multiply(
+        camera.viewProjection, mesh.transform.worldTransform.array, this._matrix)
+      this._shader.render(mesh, this._renderer)
     }
-
-    update(hitAreas: PickingHitArea[]) {
-        const oldRes = settings.RESOLUTION;
-        settings.RESOLUTION = 1;
-        this._renderer.renderTexture.bind(this._output)
-        if (this._update++ % 2 === 0) {
-            // For performance reasons, the update method alternates between rendering 
-            // the meshes and reading the pixels from the rendered texture.
-            this._renderer.renderTexture.clear()
-            for (let hitArea of hitAreas) {
-                this.renderHitArea(hitArea)
-            }
-        } else {
-            const gl = this._renderer.gl
-            gl.readPixels(0, 0, this._output.width, this._output.height, gl.RGBA, gl.UNSIGNED_BYTE, this._pixels)
-        }
-        settings.RESOLUTION = oldRes;
-        this._renderer.renderTexture.bind(undefined)
-    }
-
-    private _matrix = new Float32Array(16)
-
-    renderHitArea(hitArea: PickingHitArea) {
-        const uniforms = this._shader.uniforms
-        const meshes = hitArea.object instanceof Mesh3D ? [hitArea.object] : hitArea.object.meshes
-        const camera = hitArea.camera || Camera.main
-        for (let mesh of meshes) {
-            uniforms.u_Id = hitArea.id
-            uniforms.u_ModelViewProjection = Mat4.multiply(
-                camera.viewProjection, mesh.transform.worldTransform.array, this._matrix)
-            this._shader.render(mesh, this._renderer)
-        }
-    }
+  }
 }
