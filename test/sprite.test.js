5b767c2ff5b2812f2354603b5bf9de86c93c2582
Jens Malmborg <jnsmalm@gmail.com>
2022-08-01 22:42:21 +0200

    Added test for sprite draw order

--- test/sprite.test.js
+++ test/sprite.test.js
@@ -42,4 +42,23 @@ describe("Sprite", () => {
     expect(render).to.match(
       await getImageDataFromUrl("snapshots/ehxit.png"), { threshold: 0.11 })
   })
+
+  it("should render correctly back to front", async () => {
+    let render = await getImageDataFromRender((renderer, resources) => {
+      let container = new PIXI3D.Container3D()
+      let sprites = []
+      for (let i = 0; i < 3; i++) {
+        let sprite = new PIXI3D.Sprite3D(resources["assets/bunnies/rabbitv3.png"].texture)
+        sprite.scale.set(5)
+        sprite.position.set(0 + i * 0.5, 0, 0 + i * 1)
+        sprites.unshift(sprite)
+      }
+      sprites.forEach(sprite => container.addChild(sprite))
+      renderer.render(container)
+    }, [
+      "assets/bunnies/rabbitv3.png"
+    ])
+    expect(render).to.match(
+      await getImageDataFromUrl("snapshots/aauwp.png"), { threshold: 0.11 })
+  })
 })
