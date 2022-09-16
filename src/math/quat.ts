a930217cf367c0c4a3e2a629b629badcac85102e
Gordon Cummings <Gordon.Cummings@tombola.com>
2022-09-17 00:24:19 +0100

    Couldn't reliably test quaternions to euler
    once you begin to rotate across three seperate axis or beyond 90 degrees, the valid rotations become impossible to match in testing, its lack of reliability suggests its not ideal for a helper method that could be misused

--- src/math/quat.ts
+++ src/math/quat.ts
@@ -1,7 +1,4 @@
-import { quat, } from "gl-matrix"
-import { RAD_TO_DEG } from "@pixi/math"
-const HALF_PI = Math.PI * 0.5;
-
+import { quat } from "gl-matrix"
 export class Quat {
   static set(x: number, y: number, z: number, w: number, out = new Float32Array(4)) {
     return <Float32Array>quat.set(out, x, y, z, w)
@@ -20,35 +17,6 @@ export class Quat {
   }
   static fromEuler(x: number, y: number, z: number, out = new Float32Array(4)) {
     return <Float32Array>quat.fromEuler(out, x, y, z)
-  }
-  static toEuler(q: Float32Array, out = new Float32Array(3)) {
-    const x = q[0], y = q[1], z = q[2], w = q[3];
-    const sqx = Math.pow(x, 2);
-    const sqy = Math.pow(y, 2);
-    const sqz = Math.pow(z, 2);
-    const sqw = Math.pow(w, 2);
-    const magnitude = sqx + sqy + sqz + sqw;
-    const test = x * y + z * w;
-    if (test > 0.499 * magnitude) { // singularity at north pole
-      out[0] = 0;
-      out[1] = 2 * Math.atan2(x, w) * RAD_TO_DEG;
-      out[2] = HALF_PI * RAD_TO_DEG;
-      return out;
-    }
-    if (test < -0.499 * magnitude) { // singularity at south pole
-      out[0] = 0;
-      out[1] = -2 * Math.atan2(x, w) * RAD_TO_DEG;
-      out[2] = -HALF_PI * RAD_TO_DEG;
-      return out;
-    }
-    const bank = Math.atan2(2 * x * w - 2 * y * z, -sqx + sqy - sqz + sqw);
-    const heading = Math.atan2(2 * y * w - 2 * x * z, sqx - sqy - sqz + sqw);
-    const attitude = Math.asin(2 * test / magnitude);
-    out[0] = bank * RAD_TO_DEG;
-    out[1] = heading * RAD_TO_DEG;
-    out[2] = attitude * RAD_TO_DEG;
-
-    return out;
   }
   static conjugate(a: Float32Array, out = new Float32Array(4)) {
     return <Float32Array>quat.conjugate(out, a)
